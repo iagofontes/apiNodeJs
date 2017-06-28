beca7a20e55005190d190a1bc14c3441544e0d47
iagofontes <iagofontes@hotmail.com>
2017-06-28 12:56:14 -0300

    Retorno de status code correto, tratamento de parametro.

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -7,6 +7,19 @@ module.exports = function(app){
 
     app.post('/pagamentos/pagamento', function(req, resp){
 
+        req.assert("status", "Status deve ser preenchido").notEmpty();
+        req.assert("valor", "Valor deve ser preenchido e maior que 0")
+            .notEmpty()
+            .isFloat();
+        
+        var erros = req.validationErrors();
+
+        if(erros){
+            console.log('Problemas com validação. Erros: '+erros);
+            resp.status(400).send(erros);
+            return;
+        }
+
         var pagamento = req.body;
         var conn = app.persistencia.ConnectionFactory();
         var pagamentoDao = new app.persistencia.PagamentoDAO(conn);
@@ -14,9 +27,12 @@ module.exports = function(app){
         pagamento.data = new Date;
         pagamentoDao.salva(pagamento, function(err, result){
             if(err){
-                resp.send(err);
+                console.log('Problemas ao realizar a inserção. '+err);
+                resp.status(500).send(err);
             }else{
-                resp.json(pagamento);
+                res.location('/pagamentos/pagamento/'+
+                    result.insertId);
+                resp.status(201).json(pagamento);
             }
         });
     });
